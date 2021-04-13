ae5f6d7e988e24e4191c6910e77481e583af149a
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2021-04-12 23:51:30 -0500

    Added mobile phone detail option

--- routes/mobilePhones.js
+++ routes/mobilePhones.js
@@ -4,4 +4,10 @@ var router = express.Router();
 
 /* GET Mobile Phones */
 router.get('/', mobilePhone_controlers.mobilePhone_view_all_Page );
+/* GET detail costume page */
+router.get('/detail', mobilePhone_controlers.mobilePhone_view_one_Page);
+/* GET create costume page */
+router.get('/create', mobilePhone_controlers.mobilePhone_create_Page);
+
+
 module.exports = router;
